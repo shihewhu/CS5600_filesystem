417273f951bfad1ca10913b6388fa071a468bd4b
shihe <shihe@ccs.neu.edu>
2015-12-07 16:04:03 -0500

    update test

--- test-1.sh
+++ test-1.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
 make
+if [ -f /tmp/test1-output ]
+	then 
+		rm /tmp/test1-output
+		echo "remove test1-output"
+fi
+if [ -f /tmp/test1-$1 ]
+	then 
+		rm /tmp/test1-$1
+		echo "remove file"
+fi
 ./homework -cmdline -image foo.img << EOF > /tmp/test1-output
-get file.A /tmp/test1-file.A
+get $1 /tmp/test1-$1
 quit
 EOF
 
-cat /tmp/test1-file.A
+cat /tmp/test1-$1
 echo ""
 cat /tmp/test1-output
