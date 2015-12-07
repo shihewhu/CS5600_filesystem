c7aeec3245c711cd54f505276025439ea5b10927
shihe <shihe@ccs.neu.edu>
2015-12-07 17:08:32 -0500

    test file update

--- test-1.sh
+++ test-1.sh
@@ -1,21 +1,58 @@
 #!/usr/bin/env bash
 
 make
-if [ -f /tmp/test1-output ]
-	then 
+filename=$1
+# echo "$filename"
+echo "checking test enviroment"
+if [[ -f /tmp/test1-output ]]; then 
 		rm /tmp/test1-output
-		echo "remove test1-output"
+		# echo "remove test1-output"
 fi
-if [ -f /tmp/test1-$1 ]
-	then 
-		rm /tmp/test1-$1
-		echo "remove file"
+if [[ -f /tmp/test1-$filename ]]; then 
+		rm /tmp/test1-$filename
+		# echo "remove file"
 fi
 ./homework -cmdline -image foo.img << EOF > /tmp/test1-output
-get $1 /tmp/test1-$1
+get $filename /tmp/test1-$filename
 quit
 EOF
 
-cat /tmp/test1-$1
+echo "checking get function"
+cksum=$(cksum /tmp/test1-$filename)
+case $filename in
+	"file.A" )
+		echo "check sum is $cksum"
+		if [[ "$cksum" == "3509208153 1000 /tmp/test1-file.A" ]]; then
+			echo "get file.A succeeds"
+		else
+			echo "get file.A fails"
+		fi
+		;;
+	"file.7" )
+		echo "check sum is $cksum"
+		if [[ "$cksum" ==  "94780536 6644 /tmp/test1-file.7" ]]; then
+			echo "get file.7 succeeds"
+		else
+			echo "get file.7 fails"
+		fi
+esac
+
+# if [[ "$filename" == "file.A" ]]; then 
+# 		echo "check sum is $cksum"
+# 		if [[ "$cksum" == "3509208153 1000 /tmp/test1-file.A" ]]; then
+# 			echo "get file.A succeeds"
+# 		else
+# 			echo "get file.A fails"
+# 		fi
+# elif [[ "$filename" == "file.7" ]]; then
+# 	echo "check sum is $cksum"
+# 	if [[ "$cksum" ==  "94780536 6644 /tmp/test1-file.7" ]]; then
+# 		echo "get file.7 succeeds"
+# 	else
+# 		echo "get file.7 fails"
+# 	fi
+# fi
+
+
 echo ""
 cat /tmp/test1-output
