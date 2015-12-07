fcd23e445cc6ca8af00d5669eb8bf3aa562df111
shihe <shihe@ccs.neu.edu>
2015-12-07 17:12:49 -0500

    test file update

--- test-1.sh
+++ test-1.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-make
+# get args
 filename=$1
-# echo "$filename"
+
+echo "compiling"
+make
+
 echo "checking test enviroment"
 if [[ -f /tmp/test1-output ]]; then 
 		rm /tmp/test1-output
@@ -16,12 +19,11 @@ fi
 get $filename /tmp/test1-$filename
 quit
 EOF
-
 echo "checking get function"
 cksum=$(cksum /tmp/test1-$filename)
 case $filename in
 	"file.A" )
-		echo "check sum is $cksum"
+		# echo "check sum is $cksum"
 		if [[ "$cksum" == "3509208153 1000 /tmp/test1-file.A" ]]; then
 			echo "get file.A succeeds"
 		else
@@ -29,30 +31,12 @@ case $filename in
 		fi
 		;;
 	"file.7" )
-		echo "check sum is $cksum"
+		# echo "check sum is $cksum"
 		if [[ "$cksum" ==  "94780536 6644 /tmp/test1-file.7" ]]; then
 			echo "get file.7 succeeds"
 		else
 			echo "get file.7 fails"
 		fi
 esac
-
-# if [[ "$filename" == "file.A" ]]; then 
-# 		echo "check sum is $cksum"
-# 		if [[ "$cksum" == "3509208153 1000 /tmp/test1-file.A" ]]; then
-# 			echo "get file.A succeeds"
-# 		else
-# 			echo "get file.A fails"
-# 		fi
-# elif [[ "$filename" == "file.7" ]]; then
-# 	echo "check sum is $cksum"
-# 	if [[ "$cksum" ==  "94780536 6644 /tmp/test1-file.7" ]]; then
-# 		echo "get file.7 succeeds"
-# 	else
-# 		echo "get file.7 fails"
-# 	fi
-# fi
-
-
-echo ""
+echo "test1-output:"
 cat /tmp/test1-output
