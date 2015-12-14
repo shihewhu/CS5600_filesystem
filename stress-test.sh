f4b41018a247f290ab1cfc9d1f0bdcabe0d6dfda
shihe <shihe@ccs.neu.edu>
2015-12-13 19:40:04 -0500

    test file changed

---
+++ stress-test.sh
@@ -0,0 +1,257 @@
+#!/usr/bin/env bash
+
+#######################global variable#####################
+NOSPACE="error: No space left on device"
+quitcmd="cmd> quit"
+NOTDIR="error: Not a directory"
+NOFILE="error: No such file or directory"
+FILEEXISTS="error: File exists"
+NOTEMPTY="error: Directory not empty"
+ISDIR="error: Is a directory"
+output="/tmp/precompute"
+output2="/tmp/real"
+################## helper function ###############
+function initialize {
+	if [ -f $1 ] 
+	then
+		rm $1
+	fi
+		touch $1
+		head="read/write block size: 1000"
+		echo $head >> $1
+}
+
+function check_result {
+	var=$(diff $1 $2)
+	if [ "$var" == "" ]
+	then 
+		echo "test $3 passed"
+	else
+		echo "test $3 failed"
+		cat $output
+		echo "#######"
+		cat $output2
+	fi
+
+}
+
+
+################test ###########################
+#######################test################################
+echo "stress test for write/read"
+echo "cleaning files"
+if [[ -f "test.img" ]]; then
+	#statements
+	rm test.img
+fi
+echo "making image for test"
+./mktest test.img
+
+echo "making test files"
+#help function
+function create_file {
+	if [[ -f $2 ]]; then
+		#clean file
+		echo "" > $2
+	fi
+	for i in $(seq 1 $1) ; do
+		random_number=$(( ( RAsNDOM % 1000 )  + 1 ))
+		echo $random_number >> $2
+	done
+}
+
+len=2000
+count=1
+while [ $len -le 140000 ]
+do
+	#statements
+	echo "generating random file $count"
+	create_file $len "put-test-file.$count"
+	len=$((len + 5000))
+	count=$((count + 1))
+done
+
+count=$((count - 1))
+echo "starting test"
+
+for i in $(seq 1 $count); do
+	testing_file="put-test-file.$i"
+	output_file="/tmp/put-test-file.$i"
+	cksum1=$(cksum $testing_file)
+	./mktest test.img
+	./homework -cmdline -image test.img << EOF
+	put $testing_file
+	get $testing_file $output_file
+	quit
+EOF
+	cksum2=$(cksum $output_file)
+	IFS=' ' read -r -a cksum1 <<< "$cksum1"
+	IFS=' ' read -r -a cksum2 <<< "$cksum2"
+	ls -l put-test-file.$i
+	if [[ "${cksum1[0]}" == "${cksum2[0]}" ]]; then
+		echo "test case $i passed"
+	else
+		echo "test case $i failed"
+		echo $cksum1
+		echo $cksum2
+
+	fi
+done;
+
+# cat put-test-file.1
+rm put-test-file.*
+rm /tmp/put-test-file.*
+
+
+echo "making a empty img"
+./mktest test.img
+./homework -cmdline -image test.img << EOF > NULL
+rm file.A
+rm file.7
+rm /dir1/file.0
+rm /dir1/file.2
+rm /dir1/file.270
+rmdir dir1
+EOF
+
+echo "stress test for mknod"
+touch tmpfile
+initialize $output
+for i in `seq 1 32`;
+do
+	echo "cmd> put tmpfile tmpfile.$i" >> $output
+done
+echo "cmd> put tmpfile tmpfile.33" >> $output
+echo $NOSPACE >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+put tmpfile tmpfile.1
+put tmpfile tmpfile.2
+put tmpfile tmpfile.3
+put tmpfile tmpfile.4
+put tmpfile tmpfile.5
+put tmpfile tmpfile.6
+put tmpfile tmpfile.7
+put tmpfile tmpfile.8
+put tmpfile tmpfile.9
+put tmpfile tmpfile.10
+put tmpfile tmpfile.11
+put tmpfile tmpfile.12
+put tmpfile tmpfile.13
+put tmpfile tmpfile.14
+put tmpfile tmpfile.15
+put tmpfile tmpfile.16
+put tmpfile tmpfile.17
+put tmpfile tmpfile.18
+put tmpfile tmpfile.19
+put tmpfile tmpfile.20
+put tmpfile tmpfile.21
+put tmpfile tmpfile.22
+put tmpfile tmpfile.23
+put tmpfile tmpfile.24
+put tmpfile tmpfile.25
+put tmpfile tmpfile.26
+put tmpfile tmpfile.27
+put tmpfile tmpfile.28
+put tmpfile tmpfile.29
+put tmpfile tmpfile.30
+put tmpfile tmpfile.31
+put tmpfile tmpfile.32
+put tmpfile tmpfile.33
+quit
+EOF
+
+check_result $output $output2 "stress test for mknod"
+
+# echo "stress testing for mkdir"
+# echo "makeing empty img for test"
+# ./mktest test.img
+# ./homework -cmdline -image test.img << EOF
+# rm file.A
+# rm file.7
+# rm /dir1/file.0
+# rm /dir1/file.2
+# rm /dir1/file.270
+# rmdir dir1
+# EOF
+
+rm $output
+rm $output2
+
+echo "making a empty img"
+./mktest test.img
+./homework -cmdline -image test.img << EOF > NULL
+rm file.A
+rm file.7
+rm /dir1/file.0
+rm /dir1/file.2
+rm /dir1/file.270
+rmdir dir1
+EOF
+
+echo "stress test for mkdir"
+touch tmpfile
+initialize $output
+for i in `seq 1 32`;
+do
+	echo "cmd> mkdir dir.$i" >> $output
+done
+echo "cmd> mkdir dir.33" >> $output
+echo $NOSPACE >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+mkdir dir.1
+mkdir dir.2
+mkdir dir.3
+mkdir dir.4
+mkdir dir.5
+mkdir dir.6
+mkdir dir.7
+mkdir dir.8
+mkdir dir.9
+mkdir dir.10
+mkdir dir.11
+mkdir dir.12
+mkdir dir.13
+mkdir dir.14
+mkdir dir.15
+mkdir dir.16
+mkdir dir.17
+mkdir dir.18
+mkdir dir.19
+mkdir dir.20
+mkdir dir.21
+mkdir dir.22
+mkdir dir.23
+mkdir dir.24
+mkdir dir.25
+mkdir dir.26
+mkdir dir.27
+mkdir dir.28
+mkdir dir.29
+mkdir dir.30
+mkdir dir.31
+mkdir dir.32
+mkdir dir.33
+quit
+EOF
+
+check_result $output $output2 "stress test for mkdir"
+
+# echo "stress testing for mkdir"
+# echo "makeing empty img for test"
+# ./mktest test.img
+# ./homework -cmdline -image test.img << EOF
+# rm file.A
+# rm file.7
+# rm /dir1/file.0
+# rm /dir1/file.2
+# rm /dir1/file.270
+# rmdir dir1
+# EOF
+
+rm $output
+rm $output2
+
--- test-basic.sh
+++ test-basic.sh
@@ -10,6 +10,10 @@ quitcmd="cmd> quit"
 NOTDIR="error: Not a directory"
 NOFILE="error: No such file or directory"
 FILEEXISTS="error: File exists"
+NOTEMPTY="error: Directory not empty"
+ISDIR="error: Is a directory"
+output="/tmp/precompute"
+output2="/tmp/real"
 ################## helper function ###############
 function initialize {
 	if [ -f $1 ] 
@@ -28,7 +32,9 @@ function check_result {
 		echo "test $3 passed"
 	else
 		echo "test $3 failed"
-		echo $var
+		cat $output
+		echo "#######"
+		cat $output2
 	fi
 
 }
@@ -36,7 +42,8 @@ function check_result {
 
 ################test ###########################
 # helper function
-
+make clean
+make COVERAGE=1
 echo "creating image file for test"
 
 if [ -f "test.img" ]
@@ -44,10 +51,16 @@ then
 	./mktest test.img
 fi
 
-echo "testing ls current dir"
-output="/tmp/precompute"
-output2="/tmp/real"
 
+
+echo "initial test"
+
+
+
+
+
+./mktest test.img
+echo "testing ls current dir"
 initialize $output
 echo "cmd> ls" >> $output
 echo "dir1" >> $output
@@ -92,7 +105,32 @@ EOF
 check_result $output $output2 "ls a dir path"
 
 echo "testing ls-l current dir"
-echo "TODO"
+initialize $output
+echo "cmd> ls-l /whatever/file" >> $output
+echo $NOFILE >> $output
+echo "cmd> ls-l /file.A/file" >> $output
+echo $NOFILE >> $output
+echo "cmd> ls-l /dir1/file.11" >> $output
+echo $NOFILE >> $output
+echo "cmd> ls-l file.A" >> $output
+echo "/file.A -rwxrwxrwx 1000 1" >> $output
+echo "cmd> ls-l /dir1/" >> $output
+echo "file.0 -rwxrwxrwx 0 1" >> $output
+echo "file.2 -rwxrwxrwx 2012 2" >> $output
+echo "file.270 -rwxrwxrwx 276177 270" >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+ls-l /whatever/file
+ls-l /file.A/file
+ls-l /dir1/file.11
+ls-l file.A
+ls-l /dir1/
+quit
+EOF
+
+check_result $output $output2 "ls-l"
+
 
 echo "testing rename a file"
 initialize $output
@@ -117,7 +155,7 @@ echo "cmd> rename file.8 f" >> $output
 echo "cmd> ls" >> $output
 echo "dir1" >> $output
 echo "f" >> $output
-echo "file.A" >> $output
+echo "file.A" >> $outputecho "/file.A -rwxrwxrwx 1000 1" >> $output
 echo $quitcmd >> $output
 
 ./homework -cmdline -image test.img << EOF > $output2
@@ -148,8 +186,8 @@ check_result $output $output2 "rename file with longer name"
 
 
 echo "testing basic put file"
+touch puttest
 initialize $output
-initialize puttest
 echo "testing basic put file" > puttest
 echo "cmd> put puttest" >> $output
 echo "cmd> ls" >> $output
@@ -170,7 +208,6 @@ EOF
 
 check_result $output $output2 "baisc put file"
 
-
 echo "testing remove"
 initialize $output
 echo "cmd> rm puttest" >> $output
@@ -178,7 +215,7 @@ echo "cmd> ls" >> $output
 echo "dir1" >> $output
 echo "file.7" >> $output
 echo "file.A" >> $output
-echo "show puttest" >> $output
+echo "cmd> show puttest" >> $output
 echo $NOFILE >> $output
 echo $quitcmd >> $output
 
@@ -190,13 +227,159 @@ quit
 EOF
 
 check_result $output $output2 "remove"
-# echo "testing mkdir"
-# initialize $output
-# echo "cmd> mkdir test" >> $output
-# echo "cmd> ls" >> $output
-# echo "dir1" >> $output
-# echo "file.7" >> $output
-# echo "file.A" >> $output
-# echo "test" >> $output
-# echo "cmd> ls test" >> $output
 
+echo "testing mkdir"
+initialize $output
+echo "cmd> mkdir dir1" >> $output # bad path dir exits
+echo $FILEEXISTS >> $output
+echo "cmd> mkdir file.7" >> $output # bad path file exits
+echo $FILEEXISTS >> $output
+echo "cmd> mkdir /file.7/test/" >> $output # bath path
+echo $FILEEXISTS >> $output
+echo "cmd> mkdir /whatever/test/" >> $output # bath path
+echo $FILEEXISTS >> $output
+echo "cmd> mkdir test" >> $output
+echo "cmd> ls" >> $output
+echo "dir1" >> $output
+echo "file.7" >> $output
+echo "file.A" >> $output
+echo "test" >> $output
+echo "cmd> ls test" >> $output
+echo "cmd> cd test" >> $output
+echo "cmd> put puttest" >> $output
+echo "cmd> ls" >> $output
+echo "puttest" >> $output
+echo "cmd> show puttest" >> $output
+cat puttest >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+mkdir dir1
+mkdir file.7
+mkdir /file.7/test/
+mkdir /whatever/test/
+mkdir test
+ls
+ls test
+cd test
+put puttest
+ls
+show puttest
+quit
+EOF
+
+check_result $output $output2 "mkdir"
+
+echo "testing remove dir" 
+initialize $output
+echo "cmd> rmdir /whatever/test/" >> $output
+echo $NOFILE >> $output
+echo "cmd> rmdir /file.7/test/" >> $output
+echo $NOFILE >> $output
+echo "cmd> rmdir /dir1/test/" >> $output
+echo $NOFILE >> $output
+echo "cmd> rmdir /dir1/file.0/" >> $output
+echo $NOTDIR >> $output
+echo "cmd> rmdir test" >> $output
+echo $NOTEMPTY >> $output
+echo "cmd> rm test/puttest" >> $output
+echo "cmd> ls test" >> $output
+echo "cmd> rmdir test" >> $output
+echo "cmd> ls" >> $output
+echo "dir1" >> $output
+echo "file.7" >> $output
+echo "file.A" >> $output
+echo "cmd> ls test" >> $output
+echo $NOFILE >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+rmdir /whatever/test/
+rmdir /file.7/test/
+rmdir /dir1/test/
+rmdir /dir1/file.0/
+rmdir test
+rm test/puttest
+ls test
+rmdir test
+ls
+ls test
+quit
+EOF
+
+check_result $output $output2 "remove dir"
+
+
+echo "testing unlink"
+initialize $output
+echo "cmd> rm /whatever/file/" >> $output
+echo $NOFILE >> $output
+echo "cmd> rm /file.7/file/" >> $output
+echo $NOFILE >> $output
+echo "cmd> rm /dir1/file.11" >> $output
+echo $NOFILE >> $output
+echo "cmd> rm /dir1/" >> $output
+echo $ISDIR >> $output
+echo "cmd> rm /dir1/file.0" >> $output
+echo "cmd> ls /dir1/" >> $output
+echo "file.2" >> $output
+echo "file.270" >> $output
+echo "cmd> show /dir1/file.0" >> $output
+echo $NOFILE >> $output 
+echo "cmd> rm /dir1/file.2" >> $output
+echo "cmd> ls /dir1/" >> $output
+
+echo "file.270" >> $output
+echo "cmd> show /dir1/file.2" >> $output
+echo $NOFILE >> $output
+echo "cmd> rm /dir1/file.270" >> $output
+echo "cmd> ls /dir1/" >> $output
+echo "cmd> show /dir1/file.270" >> $output
+echo $NOFILE >> $output
+echo $quitcmd >> $output
+
+
+./homework -cmdline -image test.img << EOF > $output2
+rm /whatever/file/
+rm /file.7/file/
+rm /dir1/file.11
+rm /dir1/
+rm /dir1/file.0
+ls /dir1/
+show /dir1/file.0
+rm /dir1/file.2
+ls /dir1/
+show /dir1/file.2
+rm /dir1/file.270
+ls /dir1/
+show /dir1/file.270
+quit
+EOF
+
+check_result $output $output2 "unlink"
+
+rm $output
+rm $output2
+
+
+echo "testing chmod"
+initialize $output
+echo "cmd> chmod 754 file.A" >> $output
+echo "cmd> ls-l file.A" >> $output
+echo "/file.A -rwxr-xr-- 1000 1" >> $output
+echo $quitcmd >> $output
+
+./homework -cmdline -image test.img << EOF > $output2
+chmod 754 file.A
+ls-l file.A
+quit
+EOF
+
+check_result $output $output2 "change mode"
+
+
+#######################stress test################
+./stress-test.sh
+########################### coverage #############
+# echo "coverage is:"
+# gcov homework
